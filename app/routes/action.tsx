879c7340b7556bc593bae974f5e1422380be19c5
calmery <calmery@users.noreply.github.com>
2022-01-28 14:52:12 +0900

    読み込み中にボタンを `disabled` にする

--- app/routes/action.tsx
+++ app/routes/action.tsx
@@ -1,13 +1,21 @@
 import { useCallback } from "react";
-import { ActionFunction, Form, redirect, useFetcher } from "remix";
+import { ActionFunction, Form, useFetcher, useTransition } from "remix";
 
-export const action: ActionFunction = ({ request }) => {
+const sleep = (ms: number) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+};
+
+export const action: ActionFunction = async ({ request }) => {
   console.log(request);
+  await sleep(3000);
   return new Response(JSON.stringify({ ok: true }));
 };
 
 const Action = () => {
   const fetcher = useFetcher();
+  const transition = useTransition();
 
   const handleClick = useCallback(async () => {
     const response = await fetcher.submit(new FormData(), {
@@ -16,14 +24,20 @@ const Action = () => {
     console.log(response);
   }, [fetcher]);
 
+  const disabled = transition.state !== "idle" || fetcher.state !== "idle";
+
   return (
     <div>
       <h2>With Form</h2>
       <Form method="post">
-        <button type="submit">Submit</button>
+        <button disabled={disabled} type="submit">
+          Submit
+        </button>
       </Form>
       <h2>Without Form</h2>
-      <button onClick={handleClick}>Submit</button>
+      <button disabled={disabled} onClick={handleClick}>
+        Submit
+      </button>
     </div>
   );
 };
